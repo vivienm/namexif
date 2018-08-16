0c01b336eb1571c517072e915b4c71b99118f53f
Vivien Maisonneuve <v.maisonneuve@gmail.com>
2018-08-16 17:08:05 +0200

    Softcoded crate name in build.rs

--- build.rs
+++ build.rs
@@ -14,7 +14,7 @@ fn main() {
     let mut app = app::build_app();
     let output_dir = env::var_os("OUT_DIR").unwrap();
     fs::create_dir_all(&output_dir).unwrap();
-    app.gen_completions("namexif", Shell::Bash, &output_dir);
-    app.gen_completions("namexif", Shell::Zsh, &output_dir);
-    app.gen_completions("namexif", Shell::Fish, &output_dir);
+    app.gen_completions(crate_name!(), Shell::Bash, &output_dir);
+    app.gen_completions(crate_name!(), Shell::Zsh, &output_dir);
+    app.gen_completions(crate_name!(), Shell::Fish, &output_dir);
 }

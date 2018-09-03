f50b420f59ede4db513e36526e43795fbec0f7cb
Vivien Maisonneuve <v.maisonneuve@gmail.com>
2018-09-03 22:16:36 +0200

    Improve log format

--- src/main.rs
+++ src/main.rs
@@ -21,13 +21,22 @@ use app::build_app;
 use rename::BatchRenamer;
 use settings::Settings;
 
+fn init_logger(settings: &Settings) -> Result<(), simplelog::TermLogError> {
+    let config = simplelog::Config {
+        time: None,
+        level: Some(log::Level::Info),
+        target: None,
+        location: None,
+        time_format: None,
+    };
+    simplelog::TermLogger::init(settings.log_level, config)
+}
+
 fn main() {
     let matches = build_app().get_matches();
     let settings = Settings::from_matches(&matches);
     let source_dir = Path::new(matches.value_of("source").unwrap());
-
-    simplelog::TermLogger::init(settings.log_level, simplelog::Config::default()).unwrap();
-
+    init_logger(&settings).unwrap();
     let renamer = BatchRenamer::new(&settings);
     match renamer.run(source_dir) {
         Err(()) => exit(2),
